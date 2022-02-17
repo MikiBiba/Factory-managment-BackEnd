5a5378cffdcd790e586b35d6af9691a306d96d9a
MikiBiba <mikibiba4@gmail.com>
2022-02-17 03:55:11 +0200

    Progressing

---
+++ Factory/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using Factory.Models;
+
+using System.Web.Http.Cors;
+
+
+namespace Factory.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class UserController : ApiController
+    {
+        // GET: api/User
+
+        UserBL userBL = new UserBL();
+        public List<User> Get()
+        {
+            return userBL.GetUsers();
+        }
+
+        // GET: api/User/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/User
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/User/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/User/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Factory/Models/UserBL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Factory.Models
+{
+    public class UserBL
+    {
+        FactoryDBEntitie db = new FactoryDBEntitie();
+
+        public List<User> GetUsers()
+        {
+             List<User> users = new List<User>();
+
+            foreach (User user in db.Users)
+            {
+                users.Add(user);
+            }
+            return users;   
+        }
+
+
+
+
+    }
+}
